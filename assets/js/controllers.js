2c9cb07f930f9f0dde5201a2b82c84bb8c3a5365
Jeremy Smith <smithjeremym@gmail.com>
2016-04-05 15:08:52 -0400

    insert styleguide variables into styles.scss, get our app talking to the RAILs app

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -5,6 +5,7 @@ canIWalk.controller('walkController', ['$http', '$scope', function($http, $scope
 
   $http.get('https://obscure-lowlands-76683.herokuapp.com/').success(function(data){
     console.log("we are talking to the rails json!");
+    console.log(data);
     $scope.stuff = data;
   });
 
