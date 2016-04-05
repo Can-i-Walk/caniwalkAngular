2e5d3fd9767fb4d30018c290820fc825ec5c566e
Jeremy Smith <smithjeremym@gmail.com>
2016-04-04 21:18:24 -0400

    change directory names, wire up Angular and add html partials and scss files

---
+++ Assets/JS/app.js
@@ -0,0 +1,32 @@
+var canIWalk = angular.module("canIWalk", ['ngRoute']);
+
+canIWalk.config(function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'partials/inputDest.html'
+      // controller: 'homepageController'
+    })
+    .when('/account', {
+      templateUrl: 'partials/dashboard.html'
+      // controller: 'homepageController'
+    })
+    .when('/edit_account', {
+      templateUrl: 'partials/editAccount.html'
+      // controller: 'homepageController'
+    })
+    .when('/walk_info', {
+      templateUrl: 'partials/walkInfo.html'
+      // controller: 'homepageController'
+    })
+    .when('/your_walk', {
+      templateUrl: 'partials/duringWalk.html'
+      // controller: 'homepageController'
+    })
+    .when('/login', {
+      templateUrl: 'partials/login.html'
+      // controller: 'homepageController'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
+});
