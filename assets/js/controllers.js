b8332608e933d68502e0a38d4842f111b33caa47
Greg Turmel <gregturmel@gmail.com>
2016-04-05 15:24:40 -0400

    add rough html to input destination page

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -3,10 +3,10 @@ canIWalk.controller('walkController', ['$http', '$scope', function($http, $scope
   $scope.message = "we are talking to Angular!";
   console.log('control!');
 
-  // $http.get('http://THERAILSAPI').success(function(data){
-  //   console.log("we are talking to homepage.json!");
-  //   $scope.stuff = data;
-  // });
+  $http.get('https://obscure-lowlands-76683.herokuapp.com/').success(function(data){
+    console.log("we are talking to homepage.json!");
+    $scope.stuff = data;
+  });
 
 
 }]);
