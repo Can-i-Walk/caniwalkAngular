10b3279277363e4763b7774af46becf56e2dea02
Greg Turmel <gregturmel@gmail.com>
2016-04-06 11:53:39 -0400

    google maps works why?

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -15,3 +15,11 @@ canIWalk.controller('walkController', ['$http', '$scope', function($http, $scope
 };
 
 }]);
+// 
+// canIWalk.controller('MyController', function(NgMap) {
+//   NgMap.getMap().then(function(map) {
+//     console.log(map.getCenter());
+//     console.log('markers', map.markers);
+//     console.log('shapes', map.shapes);
+//   });
+// });
