698232dbcb93278ea84e295f5f04353e68c52a79
Jeremy Smith <smithjeremym@gmail.com>
2016-04-11 14:21:37 -0400

    fix logic error for placement of trip POST

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -56,98 +56,99 @@ function findTrip(destLatLng, destName){
         function(response, status) {
           if (status == google.maps.DirectionsStatus.OK) {
             directionsDisplay.setDirections(response);
+            distance = response.routes[0].legs[0].distance.text;
+            duration = response.routes[0].legs[0].duration.text;
+            //  this is getting the distance (in miles) and the duration (in hours & minutes) of our trip and assigning those values to variables.
+            console.log(response);
+            $(".walkInfo-distance").html(distance);
+            $(".walkInfo-duration").html(duration);
+
+            // ajax call with test data - shit works!
+            // $.ajax({
+            //   type : 'POST',
+            //   dataType : 'json',
+            //   url: 'https://peaceful-journey-51869.herokuapp.com/trips',
+            //   data: {
+            //     "user_id": 1,
+            //     "trip_name": "DBAP",
+            //     "distance": .4,
+            //     "origin_name": "The Parlour",
+            //     "origin_lat": 35.996571,
+            //     "origin_long": -78.902358,
+            //     "dest_name": "DBAP",
+            //     "dest_lat": 35.991785,
+            //     "dest_long": -78.905056
+            //   },
+            //   headers: {
+            //      contentType: "application/json",
+            //    },
+            //   success : function(data) {
+            //
+            //     console.log("this trip's info has been sent to the database");
+            //     //this GET will get the places of interest from other users based on the route data we send to the backend
+            //     $.ajax({
+            //       method: 'GET',
+            //       url: 'https://peaceful-journey-51869.herokuapp.com/places/nearby_favorite_places/?distance=.4&origin_lat=35.996571&origin_long=-78.902358&dest_lat=35.991785&dest_long=-78.905056',
+            //       success: function (data) {
+            //       console.log("successful GET");
+            //       console.log(data);
+            //       }, error: function (request,error) {
+            //       console.log("unsuccessful GET");
+            //       console.log(request);
+            //       }
+            //     });
+            //
+            //
+            //   }, error: function(request,error){
+            //     console.log(request);
+            //     console.log("error");
+            //   }
+            // });
+
+            // this posts info about the trip to the Rails backend and get nearby places of interest back!
+            $.ajax({
+              type : 'POST',
+              dataType : 'json',
+              url: 'https://peaceful-journey-51869.herokuapp.com/trips',
+              data: {
+                "user_id": 1,
+                "trip_name": destName,
+                "distance": distance,
+                "origin_name": originName,
+                "origin_lat": pos.lat,
+                "origin_long": pos.lng,
+                "dest_name": destName,
+                "dest_lat": destLatLng.lat,
+                "dest_long": destLatLng.lng
+              },
+              headers: {
+                 contentType: "application/json",
+              },
+              success : function(data) {
+
+                console.log("this trip's info has been sent to the database");
+                //this GET will get the places of interest from other users based on the route data we send to the backend
+                $.ajax({
+                  method: 'GET',
+                  url: 'https://peaceful-journey-51869.herokuapp.com/places/nearby_favorite_places/?distance='+distance+'&origin_lat='+pos.lat+'&origin_long='+pos.lng+'&dest_lat='+destLatLng.lat+'&dest_long='+destLatLng.lng,
+                  success: function (data) {
+                    console.log("successful GET");
+                    console.log(data);
+                  }, error: function (request,error) {
+                    console.log("unsuccessful GET");
+                    console.log(request);
+                  }
+                });
+              }, error: function(request,error){
+                console.log(request);
+                console.log("error");
+              }
+            });
           } else {
             console.log(status);
             window.alert("Sorry, but we can't find walking directions to " + destName);
-          }
-          distance = response.routes[0].legs[0].distance.text;
-          duration = response.routes[0].legs[0].duration.text;
-          //  this is getting the distance (in miles) and the duration (in hours & minutes) of our trip and assigning those values to variables.
-          console.log(response);
-          $(".walkInfo-distance").html(distance);
-          $(".walkInfo-duration").html(duration);
+          };
 
-          // ajax call with test data - shit works!
-          // $.ajax({
-          //   type : 'POST',
-          //   dataType : 'json',
-          //   url: 'https://peaceful-journey-51869.herokuapp.com/trips',
-          //   data: {
-          //     "user_id": 1,
-          //     "trip_name": "DBAP",
-          //     "distance": .4,
-          //     "origin_name": "The Parlour",
-          //     "origin_lat": 35.996571,
-          //     "origin_long": -78.902358,
-          //     "dest_name": "DBAP",
-          //     "dest_lat": 35.991785,
-          //     "dest_long": -78.905056
-          //   },
-          //   headers: {
-          //      contentType: "application/json",
-          //    },
-          //   success : function(data) {
-          //
-          //     console.log("this trip's info has been sent to the database");
-          //     //this GET will get the places of interest from other users based on the route data we send to the backend
-          //     $.ajax({
-          //       method: 'GET',
-          //       url: 'https://peaceful-journey-51869.herokuapp.com/places/nearby_favorite_places/?distance=.4&origin_lat=35.996571&origin_long=-78.902358&dest_lat=35.991785&dest_long=-78.905056',
-          //       success: function (data) {
-          //       console.log("successful GET");
-          //       console.log(data);
-          //       }, error: function (request,error) {
-          //       console.log("unsuccessful GET");
-          //       console.log(request);
-          //       }
-          //     });
-          //
-          //
-          //   }, error: function(request,error){
-          //     console.log(request);
-          //     console.log("error");
-          //   }
-          // });
-
-          // this posts info about the trip to the Rails backend and get nearby places of interest back!
-          $.ajax({
-            type : 'POST',
-            dataType : 'json',
-            url: 'https://peaceful-journey-51869.herokuapp.com/trips',
-            data: {
-              "user_id": 1,
-              "trip_name": destName,
-              "distance": distance,
-              "origin_name": originName,
-              "origin_lat": pos.lat,
-              "origin_long": pos.lng,
-              "dest_name": destName,
-              "dest_lat": destLatLng.lat,
-              "dest_long": destLatLng.lng
-            },
-            headers: {
-               contentType: "application/json",
-            },
-            success : function(data) {
-
-              console.log("this trip's info has been sent to the database");
-              //this GET will get the places of interest from other users based on the route data we send to the backend
-              $.ajax({
-                method: 'GET',
-                url: 'https://peaceful-journey-51869.herokuapp.com/places/nearby_favorite_places/?distance='+distance+'&origin_lat='+pos.lat+'&origin_long='+pos.lng+'&dest_lat='+destLatLng.lat+'&dest_long='+destLatLng.lng,
-                success: function (data) {
-                  console.log("successful GET");
-                  console.log(data);
-                }, error: function (request,error) {
-                  console.log("unsuccessful GET");
-                  console.log(request);
-                }
-              });
-            }, error: function(request,error){
-              console.log(request);
-              console.log("error");
-            }
-          });
       });
 
       // infoWindow.setPosition(pos);
